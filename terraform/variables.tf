81a6ed4b7eff1678445457a4928d2fd683c9a727
dimaaasex911 <dimaaasex911@gmail.com>
2022-07-02 16:47:55 +0300

    working aws_asg_elb

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,40 +1,41 @@
 variable "ami" {
- type        = string
- description = "AMI ID for the EC2 instance"
- default     = "ami-052efd3df9dad4825"
- 
- validation {
-   condition     = length(var.ami) > 4 && substr(var.ami, 0, 4) == "ami-"
-   error_message = "Please provide a valid value for variable AMI."
- }
+  type        = string
+  description = "AMI ID for the EC2 instance"
+  default     = "ami-052efd3df9dad4825"
+
+  validation {
+    condition     = length(var.ami) > 4 && substr(var.ami, 0, 4) == "ami-"
+    error_message = "Please provide a valid value for variable AMI."
+  }
 }
- 
+
 variable "type" {
- type        = string
- description = "Instance type for the EC2 instance"
- default     = "t2.micro"
- sensitive   = true
+  type        = string
+  description = "Instance type for the EC2 instance"
+  default     = "t2.micro"
+  sensitive   = true
 }
- 
+
 variable "tags" {
- type = object({
-   name = string
-   env  = string
- })
- description = "Tags for the EC2 instance"
- default = {
-   name = "My Virtual Machine"
-   env  = "Dev"
- }
-} 
+  type = object({
+    name = string
+    env  = string
+  })
+  description = "Tags for the EC2 instance"
+  default = {
+    name = "My Virtual Machine"
+    env  = "Dev"
+  }
+}
 variable "server_port" {
- type        = number
- description = "Number server port"
- default     = 8080
- sensitive   = true
+  type        = number
+  description = "Number server port"
+  default     = 8080
+  sensitive   = true
 }
 variable "subnet_id" {
-    type = string
+  type = string
+  //default = "subnet-00b40b2c0214ed2e5"
 }
 
 # data "aws_subnet_ids" "default" {
